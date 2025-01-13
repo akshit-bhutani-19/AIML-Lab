05d6174d16c4b32c78f753e5ccda224ca3eef3ec
Akki019 <Akshit.105013@stu.upes.ac.in>
2025-01-13 11:04:05 +0530

    AIML LAB Q1 Q2

---
+++ Lab1 - Python Basics/q1.py
@@ -0,0 +1,14 @@
+# Write a python program to perform basic arithmetic operations (addition. subtraction, multiplication, division. and modulus) on numbers.
+
+num1 = float(input("enter the number 1:"))
+num2 = float(input("enter the number 2:"))
+addition = num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+print(f"addition, {num1}+ {num2} = {addition}")
+print(f"substraction: {num1} - {num2} = {substraction}")
+print(f"multiplication: {num1} * {num2} = {multiplication}")
+print(f"division: {num1} / {num2} = {division}")
+print(f"modulus: {num1} - {num2} = {modulus}")
---
+++ Lab1 - Python Basics/q2.py
@@ -0,0 +1,29 @@
+# Q2. Create variables of different data types (integer, float, string, boolean) and perform basic operations on them.
+
+interger = 10
+float = 50
+boolean = True
+string = "Hello"
+
+addition = interger + float
+substraction = interger - float
+multiplication = interger * float
+division = interger / float
+modulus = interger % float
+
+print(addition)
+print(substraction)
+print(multiplication)
+print(division)
+print(modulus)
+
+concatenated_string = string + "World"
+print(concatenated_string)
+
+andop=boolean and False
+orop=boolean or False
+notop= not boolean
+
+print(andop)
+print(orop)
+print(notop)
