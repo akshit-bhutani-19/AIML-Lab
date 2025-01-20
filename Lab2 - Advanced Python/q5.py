0e93ec6ee26d5afae0fc18c8f68113d057c97480
Akki019 <Akshit.105013@stu.upes.ac.in>
2025-01-20 10:56:25 +0530

    Lab2 q5 q6

---
+++ Lab2 - Advanced Python/q5.py
@@ -0,0 +1,15 @@
+# Q5. Create a program to find the factorial of a number using a loop and conditional statements.
+
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+number = int(input("Enter a number to find its factorial: "))
+factorial_result = factorial(number)
+print(f"The factorial of {number} is: {factorial_result}")
