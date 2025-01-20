5011887f1daa399e9f9d645ba567c53de19813c1
Akki019 <Akshit.105013@stu.upes.ac.in>
2025-01-20 11:10:51 +0530

    Lab2 q7 q8

---
+++ Lab2 - Advanced Python/q7.py
@@ -0,0 +1,7 @@
+# Q7. Create a program to find the sum of all even numbers between 1 and 100.
+
+sum_of_evens = 0
+for num in range(1, 101):
+    if num % 2 == 0:
+        sum_of_evens += num
+print("The sum of all even numbers between 1 and 100 is:", sum_of_evens)
